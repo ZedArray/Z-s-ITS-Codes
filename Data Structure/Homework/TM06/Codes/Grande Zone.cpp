97802ab7317fd7dae24d1e0b15bed5b9c7976b4f
ZedArray <hafidzuddin.ra@gmail.com>
2023-03-20 12:16:37 +0700

    grande zone

--- Data Structure/Homework/TM06/Codes/Grande Zone.cpp
+++ Data Structure/Homework/TM06/Codes/Grande Zone.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(){
     deque<int> queue;
+    list<int> list;
     int n;
     cin >> n;
     for(int i = 0; i < n; i++){
@@ -10,5 +11,23 @@ int main(){
         cin >> input;
         queue.push_back(input);
     }
-    for(int i = 0;)
+    for(int i = 0; i < n; i++){
+        int adder = 0;
+        for(int j = i; j < n; j++){
+            if(queue.at(i) <= queue.at(j)){
+                adder += queue.at(i);
+            }
+            else break;
+        }
+        for(int j = i - 1; j >= 0; j--){
+            if(queue.at(i) <= queue.at(j)){
+                adder += queue.at(i);
+            }
+            else break;
+        }
+        //cout << adder << endl;
+        list.push_back(adder);
+    }
+    list.sort();
+    cout << list.back() << endl;
 }
