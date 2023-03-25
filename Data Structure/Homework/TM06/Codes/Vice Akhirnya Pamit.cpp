f3c2eebac723c9375cf7931419d0fa5026359935
ZedArray <hafidzuddin.ra@gmail.com>
2023-03-25 21:59:29 +0700

    Vice Done

--- Data Structure/Homework/TM06/Codes/Vice Akhirnya Pamit.cpp
+++ Data Structure/Homework/TM06/Codes/Vice Akhirnya Pamit.cpp
@@ -27,6 +27,7 @@ using namespace std;
 int main(){
     deque<int> input1;
     deque<int> input2;
+    deque<int> output;
     int x, y;
     cin >> x;
     cin >> y;
@@ -40,5 +41,36 @@ int main(){
         cin >> input;
         input2.push_back(input);
     }
-    
+    if(y > x){
+        swap(input1, input2);
+    }
+    int excess = 0;
+    int i1 = input1.size();
+    int i2 = input2.size();
+    for(int i = 0; i < i1; i++){
+        if(i > i2 - 1){
+            //cout << "bigger" << input1.back() + excess << endl;
+            output.push_front(input1.back() + excess);
+            input1.pop_back();
+            excess = 0;
+        }
+        else{
+            int input = input1.back() + input2.back() + excess;
+            if(input > 9){
+                input -= 10;
+                excess = 1;
+            }
+            else{
+                excess = 0;
+            }
+            //cout << "smaller" << input << endl;
+            output.push_front(input);
+            input1.pop_back();
+            input2.pop_back();
+        }
+    }
+    int size = output.size();
+    for(int i = 0; i < size; i++){
+        cout << output.at(i);
+    }
 }
