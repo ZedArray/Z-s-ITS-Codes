a80d3bf6ac02bf30d30bc34d868f48d5f3740d1d
ZedArray <hafidzuddin.ra@gmail.com>
2023-05-21 21:38:31 +0700

    revisi

--- Data Structure/Praktikum/Modul 4/Codes/TCL.cpp
+++ Data Structure/Praktikum/Modul 4/Codes/TCL.cpp
@@ -2,42 +2,82 @@
 using namespace std;
 
 int main(){
-    deque<int> queue;
-    list<int> list;
-    priority_queue<int> end;
-    int n;
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        int input;
-        cin >> input;
-        queue.push_back(input);
-    }
-    for(int i = 0; i < n; i++){
-        int adder = 0;
-        for(int j = i; j < n; j++){
-            if(queue.at(i) <= queue.at(j)){
-                // cout << queue.at(i) << endl;
-                adder += queue.at(i);
-            }
-            else break;
+    while(1){
+        deque<int> queue;
+        list<int> list;
+        priority_queue<int> end;
+        int n;
+        cin >> n;
+        // if(n == EOF){
+        //     break;
+        // }
+        for(int i = 0; i < n; i++){
+            int input;
+            cin >> input;
+            queue.push_back(input);
         }
-        for(int j = i - 1; j >= 0; j--){
-            if(queue.at(i) <= queue.at(j)){
-                // cout << queue.at(i) << endl;
-                adder += queue.at(i);
+        for(int i = 0; i < n; i++){
+            int adder = 0;
+            for(int j = i; j < n; j++){
+                if(queue.at(i) <= queue.at(j)){
+                    // cout << queue.at(i) << endl;
+                    adder += queue.at(i);
+                }
+                else break;
             }
-            else break;
+            for(int j = i - 1; j >= 0; j--){
+                if(queue.at(i) <= queue.at(j)){
+                    // cout << queue.at(i) << endl;
+                    adder += queue.at(i);
+                }
+                else break;
+            }
+            //cout << adder << endl;
+            // list.push_back(adder);
+            end.push(adder);
         }
-        //cout << adder << endl;
-        // list.push_back(adder);
-        end.push(adder);
+        //cout << endl;
+        //list.sort();
+        cout << end.top() << endl;
+        // int size = end.size();
+        // for(int i = 0; i < size; i++){
+        //     cout << end.top() << endl;
+        //     end.pop();
+        // } 
     }
-    //cout << endl;
-    //list.sort();
-    cout << end.top() << endl;
-    // int size = end.size();
-    // for(int i = 0; i < size; i++){
-    //     cout << end.top() << endl;
-    //     end.pop();
+    // int n;
+    // cin >> n;
+    // for(int i = 0; i < n; i++){
+    //     int input;
+    //     cin >> input;
+    //     queue.push_back(input);
     // }
+    // for(int i = 0; i < n; i++){
+    //     int adder = 0;
+    //     for(int j = i; j < n; j++){
+    //         if(queue.at(i) <= queue.at(j)){
+    //             // cout << queue.at(i) << endl;
+    //             adder += queue.at(i);
+    //         }
+    //         else break;
+    //     }
+    //     for(int j = i - 1; j >= 0; j--){
+    //         if(queue.at(i) <= queue.at(j)){
+    //             // cout << queue.at(i) << endl;
+    //             adder += queue.at(i);
+    //         }
+    //         else break;
+    //     }
+    //     //cout << adder << endl;
+    //     // list.push_back(adder);
+    //     end.push(adder);
+    // }
+    // //cout << endl;
+    // //list.sort();
+    // cout << end.top() << endl;
+    // // int size = end.size();
+    // // for(int i = 0; i < size; i++){
+    // //     cout << end.top() << endl;
+    // //     end.pop();
+    // // }
 }
