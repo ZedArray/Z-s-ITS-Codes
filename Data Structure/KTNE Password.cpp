7c3b0e63e5320398730dcda13989b6ffc18cfc85
ZedArray <hafidzuddin.ra@gmail.com>
2023-05-23 12:12:48 +0700

    bruh

---
+++ Data Structure/KTNE Password.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* 
+        "about",    
+        "after",    
+        "again",    
+        "below",    
+        "could",
+        "every",    
+        "first",    
+        "found",    
+        "great",    
+        "house",
+        "large",    
+        "learn",    
+        "never",    
+        "other",    
+        "place",
+        "plant",    
+        "point",    
+        "right",    
+        "small",    
+        "sound",
+        "spell",    
+        "still",    
+        "study",    
+        "their",    
+        "there",
+        "these",    
+        "thing",    
+        "think",    
+        "three",    
+        "water",
+        "where",    
+        "which",    
+        "world",    
+        "would",    
+        "write"
+ */
+int main(){
+    string passwords[5][35];
+    passwords[0][0] = "a";
+    passwords[0][1] = "b";
+    passwords[0][2] = "o";
+    passwords[0][3] = "u";
+    passwords[0][4] = "t";
+
+    passwords[1][0] = "a";
+    passwords[1][1] = "f";
+    passwords[1][2] = "t";
+    passwords[1][3] = "e";
+    passwords[1][4] = "r";
+
+    passwords[2][0] = "a";
+    passwords[2][1] = "g";
+    passwords[2][2] = "a";
+    passwords[2][3] = "i";
+    passwords[2][4] = "n";
+
+    passwords[3][0] = "b";
+    passwords[3][1] = "e";
+    passwords[3][2] = "l";
+    passwords[3][3] = "o";
+    passwords[3][4] = "w";
+
+    passwords[4][0] = "c";
+    passwords[4][1] = "o";
+    passwords[4][2] = "u";
+    passwords[4][3] = "l";
+    passwords[4][4] = "d";
+
+    passwords[5][0] = "e";
+    passwords[5][1] = "v";
+    passwords[5][2] = "e";
+    passwords[5][3] = "r";
+    passwords[5][4] = "y";
+
+    passwords[6][0] = "f";
+    password
+
+}
