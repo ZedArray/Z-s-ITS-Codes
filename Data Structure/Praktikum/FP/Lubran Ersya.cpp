1e1c850f0b93a9aa7b38da8675416c38f6fa9ac2
ZedArray <hafidzuddin.ra@gmail.com>
2023-06-05 14:01:16 +0700

    FP

---
+++ Data Structure/Praktikum/FP/Lubran Ersya.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string input;
+    stack<char> st;
+    cin >> input;
+    char temp;
+    for(int i = 0; i < input.length(); i++){
+        if(input.at(i) == '(' || input[i] == '{' || input[i] == '['){
+            st.push(input.at(i));
+            continue;
+        }
+        if(input.empty()){
+            cout << "unvaldi" << endl;
+        }
+        if(input.at(i) == ')'){
+            temp = st.top();
+            st.pop();
+            if(temp != '('){
+                cout << "unvaldi" << endl;
+                break;
+            }
+        }
+        else if(input.at(i) == '}'){
+            temp = st.top();
+            st.pop();
+            if(temp != '{'){
+                cout << "unvaldi" << endl;
+                break;
+            }
+        }
+        else if(input.at(i) == ']'){
+            temp = st.top();
+            st.pop();
+            if(temp != '['){
+                cout << "unvaldi" << endl;
+                break;
+            }
+        }
+    }
+    if(st.empty()){
+        cout << "valdi" << endl;
+    }
+    // else{
+    //     cout << "unvaldi" << endl;
+    // }
+}
