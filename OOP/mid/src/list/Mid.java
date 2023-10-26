999997cfbd70958d633ad6b19759e112ee063284
ZedArray <hafidzuddin.ra@gmail.com>
2023-10-26 22:16:32 +0700

    midtest

--- OOP/mid/src/list/Mid.java
+++ OOP/mid/src/list/Mid.java
@@ -49,34 +49,103 @@ public class Mid extends List {
     }
 
     // 11
-    static List positives(List a) {
+	static List positives(List a){
+		List temp = new List();
+		return positives(a, temp);
+	}
+    static List positives(List a, List temp) {
     	// Your code is in here
-    	List tempList = new List();
-    	if (a.empty()) return tempList;
+    	if (a.empty()) return temp;
     	else if (a.head() >= 0) {
-    		tempList = ListOps.addtoendr(tempList, a.head());
+    		temp = ListOps.addtoendr(temp, a.head());
     	}
-    	tempList = positives(a.tail());
-    	return tempList;
+		temp = positives(a.tail(), temp);
+    	return temp;
     }
 
     // 12
     static boolean sorted(List a) {
     	// Your code is in here
+		boolean defState = false;
+    	if (a.tail.empty()) return true;
+		else if (a.head() <= a.tail.head()){
+			// System.out.println("\n" + a.head() + " " + a.tail.head());
+			defState = sorted(a.tail());
+		}
+    	return defState;
     }
     
     // 13
-    static List merge(List a, List b) {
+	static List merge(List a, List b){
+		List tempList = new List();
+		return merge(a, b, tempList);
+	}
+    static List merge(List a, List b, List temp) {
     	// Your code is in here
+		// ListOps.printList(temp);
+		// System.out.println("\n");
+		if (a.empty() && b.empty()) return temp;
+		else if (a.empty()){
+			temp = ListOps.addtoendr(temp, b.head());
+			temp = merge(a, b.tail(), temp);
+		}
+		else if (b.empty()){
+			temp = ListOps.addtoendr(temp, a.head());
+			temp = merge(a.tail(), b.tail(), temp);
+		}
+		else if (a.head() >= b.head()){
+			temp = ListOps.addtoendr(temp, b.head());
+			temp = merge(a, b.tail(), temp);
+		}
+		else if (a.head < b.head()){
+			temp = ListOps.addtoendr(temp, a.head());
+			temp = merge(a.tail(), b, temp);
+		}
+		// ListOps.printList(temp);
+		// System.out.println("\n");
+		return temp;
     }
 
     // 14
-    static List removeDuplicate(List a) {
+	static List removeDuplicate(List a){
+		return removeDuplicate(a.tail(), new List(a.head(), List.nil()), a.head());
+	}
+    static List removeDuplicate(List a, List temp, int prev) {
     	// Your code is in here
+		// System.out.println("\nOriginal List");
+		// ListOps.printList(a);
+		// System.out.println("\nto be List");
+		// ListOps.printList(temp);
+		if(a.empty()){
+			return temp;
+		}
+		else if(prev != a.head()){
+			temp = ListOps.addtoendr(temp, a.head());
+			prev = a.head();
+			temp = removeDuplicate(a.tail(), temp, prev);
+		}
+		else {
+			temp = removeDuplicate(a.tail(), temp, prev);
+		}
+		return temp;
     }
     
     // 15
-    static List skip(int x, List a) {
+	static List skip(int x, List a){
+		return skip(x, a, new List());
+	}
+    static List skip(int x, List a, List temp) {
     	// Your code is in here
+		if (a.empty()){
+			return temp;
+		}
+		else if (a.head() == x){
+			temp = skip(x, a.tail(), temp);
+		}
+		else if (a.head() != x){
+			temp = ListOps.addtoendr(temp, a. head());
+			temp = skip(x, a.tail(), temp);
+		}
+		return temp;
     }
 }   
