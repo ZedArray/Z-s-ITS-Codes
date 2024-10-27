0e89dde2e38bd7038a73f2c8bcc9a28ecddf68b8
ZedArray <hafidzuddin.ra@gmail.com>
2024-10-27 17:31:13 +0700

    aaa

---
+++ Framework Prog/GO/main.go
@@ -0,0 +1,39 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	// for i := 0; i < 10; i++ {
+// 	// 	fmt.Println(i)
+// 	// }
+// 	var num int
+// 	fmt.Scan(&num)
+// 	fmt.Println(num)
+
+// 	var firstname string
+// 	var lastname string
+// 	var email string
+// 	var tickets_bought int
+// 	var tickets_remaining int = 50
+
+// 	fmt.Println("Insert your first name")
+// 	fmt.Scan(&firstname)
+
+// 	fmt.Println("Insert your last name")
+// 	fmt.Scan(&lastname)
+
+// 	fmt.Println("Insert your email")
+// 	fmt.Scan(&email)
+
+// 	fmt.Println("Insert the amount of tickets you will buy")
+// 	fmt.Scan(&tickets_bought)
+
+// 	fmt.Printf("Welcome %v %v with the email of %v. You bought %v tickets\n", firstname, lastname, email, tickets_bought)
+
+// 	tickets_remaining -= tickets_bought
+
+// 	fmt.Println(tickets_remaining, " tickets remaining")
+
+// }
